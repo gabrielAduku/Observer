a38f8035003cba5b5a4ba964bb735e99c4796885
Gabriel <gabriel.aduculesei@gmail.com>
2021-04-23 00:54:42 +0100

    summary css implemented

--- scripts/details.js
+++ scripts/details.js
@@ -7,6 +7,8 @@ var trackerUrls = chrome.extension.getBackgroundPage().trackerUrls;
 document.getElementById("trackersFound").innerHTML = 'Observer found <span class="highlight">' + trackersFound + '</span> trackers on this website.';
 document.getElementById("trackersBlocked").innerHTML = 'Observer blocked <span class="highlight">' + trackersBlocked + '</span> trackers on this website.';
 
+var maxLength = 35;
+
 for (var i = 0; i < trackerUrls.length; i++)
 {
   var checkbox = document.createElement("input");
@@ -15,9 +17,17 @@ for (var i = 0; i < trackerUrls.length; i++)
   checkbox.value = "block";
   checkbox.id = "cb" + i;
 
+  // Shorten strings for now
+  var trackerString = trackerUrls[i];
+  if (trackerString.length > maxLength)
+  {
+    trackerString = trackerString.substr(0, maxLength) + "...";
+  }
+
   var label = document.createElement("label");
-  label.htmlFor = "id";
-  label.appendChild(document.createTextNode(trackerUrls[i] + "\n"));
+  label.htmlFor = checkbox.id;
+  label.appendChild(document.createTextNode(trackerString));
+  label.innerHTML += "<br/>";
 
   infoBox.appendChild(checkbox);
   infoBox.appendChild(label);
